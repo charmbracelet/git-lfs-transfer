9a83974fe38cc2ca3ac8ec9de9067cd6252af9de
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-10-26 14:38:36 -0400

    feat: support batch response error message

--- transfer/batch.go
+++ transfer/batch.go
@@ -9,4 +9,10 @@ type BatchItem struct {
 
 	// Args is an optional oid-line key-value pairs.
 	Args Args
+<<<<<<< HEAD
+=======
+
+	// Error is an optional error message.
+	Error string
+>>>>>>> 29d6979f6467 (feat: support batch response error message)
 }
